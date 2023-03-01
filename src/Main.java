a03a00d43e6b2e26e02d71384d010fa93b7875a6
JosiahElder <@JosiahElder>
2023-03-01 01:02:07 -0800

    Next Commit

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,22 @@ import java.awt.*;
 
 public class Main extends JFrame
 {
-    private JPanel panel1, panel2;
-    private JButton button1, button2;
-    private JLabel label1, label2;
+    private JPanel JPanel1, JPanel2;
+    private JButton JButton1, JButton2;
+    private JLabel JLabel1, JLabel2;
 
-    public Main() {
+    public Main()
+    {
+        JPanel1 = new JPanel();
+        JPanel2 = new JPanel();
+
+        setLayout(new BorderLayout());
+
+        add(JPanel1, BorderLayout.WEST);
+        add(JPanel2, BorderLayout.EAST);
+
+        JPanel1.setBackground(Color.BLUE);
+        JPanel2.setBackground(Color.RED);
     }
 
     public static void main(String[] args)
