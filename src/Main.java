eb7df2bbf077fbe42ea2e9eca0bdefb1313b8c88
JosiahElder <@JosiahElder>
2023-03-01 01:28:48 -0800

    Next Commit

--- src/Main.java
+++ src/Main.java
@@ -12,30 +12,29 @@ public class Main extends JFrame
         JPanel1 = new JPanel();
         JPanel2 = new JPanel();
 
-        JPanel1.setLayout(new BoxLayout(JPanel1, BoxLayout.Y_AXIS));
-        JPanel2.setLayout(new BoxLayout(JPanel2, BoxLayout.Y_AXIS));
+        setLayout(new BorderLayout());
 
-        add(JPanel1, BorderLayout.WEST);
-        add(JPanel2, BorderLayout.EAST);
-
-        JPanel1.setBackground(Color.BLUE);
-        JPanel2.setBackground(Color.RED);
-
-        setSize(400, 200);
+        add(JPanel1, BorderLayout.NORTH);
+        add(JPanel2, BorderLayout.CENTER);
 
         JButton1 = new JButton("Toggle Left");
         JButton1.setPreferredSize(new Dimension(150, 50));
-        JPanel1.add(JButton1);
 
         JButton2 = new JButton("Toggle Right");
         JButton2.setPreferredSize(new Dimension(150, 50));
-        JPanel2.add(JButton2);
 
-        JLabel1 = new JLabel("This is the left Pane");
-        JLabel2 = new JLabel("This is the right Pane");
+        JLabel1 = new JLabel("This is the Left Pane");
+        JLabel2 = new JLabel("This is the Right Pane");
 
-        JPanel1.add(JLabel1);
+        JPanel1.setBackground(Color.LIGHT_GRAY);
+        JPanel2.setBackground(Color.WHITE);
+
+        JPanel1.add(JButton1);
+        JPanel1.add(JButton2);
+        JPanel2.add(JLabel1);
         JPanel2.add(JLabel2);
+
+        setSize(400, 200);
 
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
