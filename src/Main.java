ab9afe6055a9727e3ce39c554fb58804a8329565
JosiahElder <@JosiahElder>
2023-03-01 00:51:14 -0800

    Next Commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import javax.swing.*;
+import java.awt.*;
+
+public class Main extends JFrame
+{
+    private JPanel panel1, panel2;
+    private JButton button1, button2;
+    private JLabel label1, label2;
+
+    public Main() {
+    }
+
+    public static void main(String[] args)
+    {
+        Main m = new Main();
+        m.setVisible(true);
     }
 }
